c33db37a15fcb738f92671919543ed9a57cd2723
Tanyeem <tanyeemsafwan123@gmail.com>
2022-01-07 21:21:53 +0600

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/hunger_equity/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.myapplication.hunger_equity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.Layout;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*RelativeLayout relativeLay= (RelativeLayout) findViewById(R.id.rel);
+        relativeLay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });*/
+        NextActivity();
+
+
+    }
+    public void NextActivity()
+    {
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+
+                Intent Intent = new Intent(MainActivity.this,Login.class);
+                MainActivity.this.startActivity(Intent);
+                MainActivity.this.finish();
+            }
+        }, 1300);
+
+    }
+}
