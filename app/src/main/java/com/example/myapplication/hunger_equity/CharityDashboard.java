7d7d0caacb1e94cf597d38b5614a03baf8a87aa1
Tanyeem <tanyeemsafwan123@gmail.com>
2022-01-17 03:14:06 +0600

    Combine 1.1

--- app/src/main/java/com/example/myapplication/hunger_equity/CharityDashboard.java
+++ app/src/main/java/com/example/myapplication/hunger_equity/CharityDashboard.java
@@ -10,5 +10,8 @@ public class CharityDashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_charity_dashboard);
+
+
+
     }
 }
